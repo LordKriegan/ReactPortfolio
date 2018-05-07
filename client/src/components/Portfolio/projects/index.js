5d4356c2e8957e68ecae32524c8bc8651948df03
Qamar Stationwala <YoPappy0@gmail.com>
2018-05-07 05:35:54 -0700

    left instructions on how to add new projects to portfolio in /client/src/components/Portfolio/projects/index.js

--- client/src/components/Portfolio/projects/index.js
+++ client/src/components/Portfolio/projects/index.js
@@ -10,3 +10,12 @@ export default [
         component: team3j
     }
 ];
+
+/* 
+    to add a new project to portfolio:
+    1. copy any existing project.
+    2. replace images/image.png with a screenshot of project (sized to 500x350px).
+    3. edit project object in index.js of the project as needed.
+    4. import project folder above.
+    5. add new project to exported array.
+*/
