6c9230140884daa313c37afae4d9d93107e8c2e0
Qamar Stationwala <YoPappy0@gmail.com>
2018-11-26 06:00:35 -0800

    added askanon app to portfolio

--- client/src/components/Portfolio/projects/projectList.js
+++ client/src/components/Portfolio/projects/projectList.js
@@ -4,7 +4,7 @@ export default [
         liveLink: "https://digi-tome.herokuapp.com",
         ghLink: "https://github.com/dtstrange/digi-tome/",
         summary: "A self-publishing site for aspiring authors to share their creative works. Users can sign up for an account and either browse and read books posted by other users or share some of their own.",
-        tech: ["Node", "Express", "React", "MySQL", "Sequelize", "Bootstrap", "Axios", "Express-jwt", "Express-fileupload", "Disqus"],
+        tech: ["Node", "Express", "React", "MySQL", "Sequelize", "Bootstrap", "Axios", "Express-jwt", "Express-fileupload", "Disqus", "Bootstrap 3.3.7"],
         role: "My roles in this project included setting up the database and setting up the crud routes, as well as figuring out how to handle file uploads. I also helped design some of the front-end React components.",
         image: require('./images/digitome.png')
     },
@@ -13,7 +13,7 @@ export default [
         liveLink: "https://team3j.herokuapp.com",
         ghLink: "https://github.com/lordkriegan/team3j/",
         summary: "A site built for a local business. Shows weekly event information, provides map and directions to shop, most recents posts from social media feeds, and site-owner can maintain a buylist (store is a collectible card shop) as well as post news on the home page.",
-        tech: ["Node", "Express", "Handlebars", "MySQL", "Sequelize","Bootstrap", "jQuery", "Axios", "Social media APIs (Twitter, Instagram, Facebook)"],
+        tech: ["Node", "Express", "Handlebars", "MySQL", "Sequelize","Bootstrap", "jQuery", "Axios", "Social media APIs (Twitter, Instagram, Facebook)", "Bootstrap 3.3.7"],
         role: "This was a solo project.",
         image: require('./images/team3j.png')
     },
@@ -31,9 +31,18 @@ export default [
         liveLink: "autumn-medical-group.herokuapp.com",
         ghLink: "https://www.github.com/lordkriegan/autumnmedical",
         summary: "A site for my cousin’s medical practice. Shows various information about practice and staff. Uses Google Maps to generate directions to the office, and Calendly to schedule appointments.",
-        tech: ["Node", "Express", "Axios", "React", "React Google Maps", "React Slick", "Recompose"],
+        tech: ["Node", "Express", "Axios", "React", "React Google Maps", "React Slick", "Recompose", "Bootstrap 3.3.7"],
         role: "This is a solo project.",
         image: require('./images/autumnmedical.png')
+    },
+    {
+        title: "Ask Anon",
+        liveLink: "askanon.herokuapp.com",
+        ghLink: "https://github.com/lordkriegan/qanda",
+        summary: "An app designed for my students to ask questions anonymously. I created it when I started wondering if students weren't speaking up because they didn't want to be embarrased.",
+        tech: ["Node", "Express", "Axios", "jQuery", "HTML", "CSS", "Bootstrap 4"],
+        role: "This is a solo project. Idea given to me by Roger Le.",
+        image: require('./images/askanon.png')
     }
 ]
 
